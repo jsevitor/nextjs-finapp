bbdb091e0c3294ef83f7ed62c12645aa435c3658
jsevitor <vitorjseo@gmail.com>
2025-09-05 09:57:10 -0300

    style(common): faz ajustes de estilo nos componentes Header e HeaderTitle

--- src/app/components/common/Header.tsx
+++ src/app/components/common/Header.tsx
@@ -6,7 +6,7 @@ type HeaderProps = {
 export default function Header({ children, className }: HeaderProps) {
   return (
     <header
-      className={`flex items-center justify-between w-full mb-4 ${className}`}
+      className={`flex items-center justify-between w-full mb-3 mt-2 ${className}`}
     >
       {children}
     </header>
--- src/app/components/common/HeaderTitle.tsx
+++ src/app/components/common/HeaderTitle.tsx
@@ -1,7 +1,13 @@
 type HeaderTitleProps = {
   title: string;
+  children?: React.ReactNode;
 };
 
-export default function HeaderTitle({ title }: HeaderTitleProps) {
-  return <h1 className="text-4xl font-bold">{title}</h1>;
+export default function HeaderTitle({ title, children }: HeaderTitleProps) {
+  return (
+    <div className="flex items-end gap-4">
+      <h1 className="text-4xl font-bold">{title}</h1>
+      <span>{children}</span>
+    </div>
+  );
 }
