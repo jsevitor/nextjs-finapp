50b0ecaf66d60dcda16207bb9fac1b095976529d
jsevitor <vitorjseo@gmail.com>
2025-09-07 09:58:00 -0300

    feat(front): cria página para listar os cartões de crédito

--- src/app/cartoes/page.tsx
+++ src/app/cartoes/page.tsx
@@ -1,37 +1,77 @@
 "use client";
 
-import { DialogAdd } from "../components/layout/DialogAdd";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import { CreditCard } from "lucide-react";
+
+import PageContainer from "../components/layout/PageContainer";
 import Header from "../components/common/Header";
 import HeaderTitle from "../components/common/HeaderTitle";
-import PageContainer from "../components/layout/PageContainer";
-import { CreditCard } from "lucide-react";
+import { ButtonVariant } from "../components/common/ButtonVariant";
 import { FiltersContainer } from "../components/filters/FiltersContainer";
-import { DataTable } from "../components/common/DataTable";
-import { Column } from "../types/tableColumns";
+import { CreditCardItem } from "../components/ui/card/CreditCardItem";
+import CardModal from "../components/ui/card/CardModal";
+
+import { useCardStore, Card } from "@/stores/cardStore";
+
+import NubankLogo from "@/../public/logos/nubank.png";
+import MercadoPagoLogo from "@/../public/logos/mercado-pago.png";
 
 export default function CardsPage() {
-  const cards = [
-    {
-      id: 1,
-      name: "Cartão 1",
-      closingDate: "31",
-      dueDate: "8",
-    },
-    {
-      id: 2,
-      name: "Cartão 2",
-      closingDate: "15",
-      dueDate: "20",
-    },
-  ];
+  const { data: session } = useSession();
+  const { cards, fetchCards, addCard, updateCard, removeCard } = useCardStore();
 
-  const columns: Column<(typeof cards)[0]>[] = [
-    { key: "name", label: "Nome" },
-    { key: "closingDate", label: "Dia de Fechamento" },
-    { key: "dueDate", label: "Dia de Vencimento" },
-  ];
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [cardBeingEdited, setCardBeingEdited] = useState<Card | null>(null);
+
+  useEffect(() => {
+    fetchCards();
+  }, [fetchCards]);
+
+  const cardStyles: Record<string, { color: string; logo: any }> = {
+    Nubank: {
+      color: "#8612D2",
+      logo: NubankLogo,
+    },
+    "Mercado Pago": {
+      color: "#55B6EA",
+      logo: MercadoPagoLogo,
+    },
+  };
+
+  const handleOpenAddModal = () => {
+    setCardBeingEdited({
+      id: "",
+      name: "",
+      closingDate: 1,
+      dueDay: 1,
+      userId: session?.user?.id || "",
+    });
+    setModalIsOpen(true);
+  };
+
+  const handleEditCard = (card: Card) => {
+    setCardBeingEdited(card);
+    setModalIsOpen(true);
+  };
+
+  const handleDeleteCard = async (id: string | undefined) => {
+    if (!id) return;
+    await removeCard(id);
+  };
+
+  const handleSubmit = async (card: Card) => {
+    if (!card.userId) return;
+
+    if (card.id) {
+      await updateCard(card); // Edição
+    } else {
+      await addCard(card); // Adição
+    }
+
+    setModalIsOpen(false);
+    setCardBeingEdited(null);
+  };
 
   return (
     <PageContainer>
@@ -39,34 +79,38 @@ export default function CardsPage() {
         <HeaderTitle title="Cartões">
           <CreditCard className="h-8 w-8" />
         </HeaderTitle>
-        <DialogAdd title="Adicionar Cartão">
-          <div className="grid grid-cols-2 gap-4">
-            <div className="grid gap-3">
-              <Label htmlFor="date-1">Nome</Label>
-              <Input id="name-1" name="name" />
-            </div>
-            <div className="grid gap-3">
-              <Label htmlFor="closingDay-1">Dia de Fechamento</Label>
-              <Input id="closingDay-1" name="closingDay" type="number" />
-            </div>
-            <div className="grid gap-3">
-              <Label htmlFor="dueDay-2">Dia de Vencimento</Label>
-              <Input id="dueDay-2" name="dueDay" type="number" />
-            </div>
-          </div>
-        </DialogAdd>
+
+        <ButtonVariant typeAction="add" action={handleOpenAddModal} />
+
+        <CardModal
+          isOpen={modalIsOpen}
+          card={cardBeingEdited}
+          onChange={setCardBeingEdited}
+          onClose={() => setModalIsOpen(false)}
+          onSubmit={handleSubmit}
+        />
       </Header>
 
-      <FiltersContainer></FiltersContainer>
+      <FiltersContainer />
 
-      <div className="border rounded-2xl overflow-hidden">
-        <DataTable
-          caption="Lista de Receitas"
-          columns={columns}
-          data={cards}
-          onEdit={(row) => console.log("Editando", row)}
-          onDelete={(row) => console.log("Deletando", row)}
-        />
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+        {cards.map((card) => {
+          const { color, logo } = cardStyles[card.name] ?? {
+            color: "#ccc",
+            logo: null,
+          };
+
+          return (
+            <CreditCardItem
+              key={card.id}
+              card={card}
+              color={color}
+              logo={logo}
+              onEdit={() => handleEditCard(card)}
+              onDelete={() => handleDeleteCard(card.id)}
+            />
+          );
+        })}
       </div>
     </PageContainer>
   );
