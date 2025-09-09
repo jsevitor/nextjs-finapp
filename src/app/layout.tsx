f0e78c5460a2e1baea08e84170dcdf9bf0998920
jsevitor <vitorjseo@gmail.com>
2025-09-09 09:16:00 -0300

    chore(front): realiza ajustes no componente root layout, otimiza na página Home e atualiza variáveis de estilo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
 import { Red_Hat_Display } from "next/font/google";
 import "./globals.css";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { AppSidebar } from "./components/layout/sidebar/AppSidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { AppSidebar } from "./components/layout/AppSidebar";
+import AuthSessionProvider from "@/components/auth/SessionProvider";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 
 const redHatDisplay = Red_Hat_Display({
   variable: "--font-red-hat-display",
@@ -15,21 +18,26 @@ export const metadata: Metadata = {
   description: "A personal finance app",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await getServerSession(authOptions);
+
   return (
     <html lang="en">
       <body className={`${redHatDisplay.className} antialiased`}>
-        <SidebarProvider>
-          <AppSidebar />
-          <main>
-            <SidebarTrigger />
-            {children}
-          </main>
-        </SidebarProvider>
+        <AuthSessionProvider>
+          {session ? (
+            <SidebarProvider>
+              <AppSidebar />
+              <main className="w-full p-4 2xl:px-8">{children}</main>
+            </SidebarProvider>
+          ) : (
+            <main className="w-full p-4 2xl:px-8">{children}</main>
+          )}
+        </AuthSessionProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+"use client";
+
+import { LayoutDashboard } from "lucide-react";
+import Header from "./components/common/Header";
+import HeaderTitle from "./components/common/HeaderTitle";
+import PageContainer from "./components/layout/PageContainer";
+import CategoriesCard from "./components/dashboard/CategoriesCard";
+
 export default function Home() {
   return (
-    <div>
-      <h1>HELLO</h1>
-    </div>
+    <PageContainer>
+      <Header>
+        <HeaderTitle title="Dashboard">
+          <LayoutDashboard className="h-8 w-8" />
+        </HeaderTitle>
+      </Header>
+      <CategoriesCard />
+    </PageContainer>
   );
 }
