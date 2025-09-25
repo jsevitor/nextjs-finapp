d71eb9f99947d534eff39da43b50d9463198ff58
jsevitor <vitorjseo@gmail.com>
2025-09-25 10:39:00 -0300

    styles(page): adequações de estilos para na página de login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -13,11 +13,11 @@ export default function LoginPage() {
 
   return (
     <div className="container mx-auto h-screen flex flex-col lg:flex-row justify-center items-center">
-      <div className="border bg-primary lg:h-1/2 rounded-t-2xl lg:rounded-t-none lg:rounded-l-2xl shadow-md lg:w-1/3 flex flex-col items-center justify-center gap-4 p-8 text-white">
+      <div className="border bg-primary h-1/5 lg:h-1/2 rounded-t-2xl lg:rounded-t-none lg:rounded-l-2xl lg:shadow-md w-1/2 lg:w-1/3 flex flex-col items-center justify-center gap-4 p-8 text-white">
         <h1 className="font-bold lg:text-4xl">FINAPP</h1>
       </div>
 
-      <div className="border h-1/3 lg:h-1/2 rounded-b-2xl lg:rounded-b-none lg:rounded-r-2xl shadow-md lg:w-1/3 flex flex-col items-center justify-center gap-4 p-8">
+      <div className="border h-1/5 lg:h-1/2 rounded-b-2xl lg:rounded-b-none lg:rounded-r-2xl shadow-md w-1/2 lg:w-1/3 flex flex-col items-center justify-center gap-4 p-8">
         <h2 className="font-bold lg:text-2xl">LOGIN</h2>
 
         <Button onClick={() => signIn("google")}>Entrar com Google</Button>
